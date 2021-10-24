55acd58de0addef51cfbf7815b23a35071e8a211
Ratul Maharaj <ratulmaharaj@gmail.com>
2021-10-24 12:38:13 +0200

    Added selenium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from instapy import InstaPy
+from selenium import webdriver
 
 # load environment variables
 load_dotenv()
@@ -9,16 +9,6 @@ load_dotenv()
 username = os.getenv("USERNAME")
 pwd = os.getenv("PASSWORD")
 
-# Start bot
-session = InstaPy(username=username, password=pwd)
-session.login()
-session.set_dont_like(["naked", "nsfw"])
-# session.set_do_like(enabled=True, percentage=67)
-session.set_user_interact(amount=2, randomize=True, percentage=60, media="Photo")
-session.follow_user_followers(
-    ["istoreza", "uctjustkidding"],
-    amount=30,
-    randomize=False,
-    interact=True,
-)
-session.end()
+driver = webdriver.Firefox()
+
+driver.get("https://instagram.com")
